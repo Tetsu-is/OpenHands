027ebb36d3badf292c2943951891f8ce655a00f6
Tetsu-is <ca02481@cartahd.com>
2025-04-01 16:21:58 +0900

    set none explicitly

--- openhands/resolver/resolve_issue.py
+++ openhands/resolver/resolve_issue.py
@@ -662,14 +662,14 @@ def main() -> None:
     logger.info(f'[LOG] Base container image: {base_container_image}')
 
     runtime_container_image = my_args.runtime_container_image
-    # if runtime_container_image is None and not my_args.is_experimental:
-    #     # runtime_container_image = (
-    #     #     f'ghcr.io/all-hands-ai/runtime:{openhands.__version__}-nikolaik'
-    #     # )
-    #     logger.info(
-    #         'runtime_container_image is None and not my_args.is_experimental then intentionally set to None'
-    #     )
-    #     runtime_container_image = None
+    if runtime_container_image is None and not my_args.is_experimental:
+        # runtime_container_image = (
+        #     f'ghcr.io/all-hands-ai/runtime:{openhands.__version__}-nikolaik'
+        # )
+        logger.info(
+            'runtime_container_image is None and not my_args.is_experimental then intentionally set to None'
+        )
+        runtime_container_image = None
 
     parts = my_args.selected_repo.rsplit('/', 1)
     if len(parts) < 2:
